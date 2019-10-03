ed3a34f518cde6749f492baf430b97fcb1f6a673
Graham Lee <graham.lee@cs.ox.ac.uk>
2019-10-03 16:39:09 +0100

    Remove need to specify username in the deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-if [ "$1" = "" ]; then
-    echo "Usage: $0 username"
-    exit 1
+USER=$1
+if [ "$USER" = "" ]; then
+    USER=$LOGNAME
 fi
 
-USER=$1
 HOST=linux2.cs.ox.ac.uk
 DIR=/fs/website/projects/RSE/
 
